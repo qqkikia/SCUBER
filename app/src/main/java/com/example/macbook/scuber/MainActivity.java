7455291e5c4722d3eb37ad2088dc43b33cc14867
Mohamed Ali Addi <qqkikia@gmail.com>
2017-06-12 17:35:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/macbook/scuber/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.macbook.scuber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+DatabaseReference dbr = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DatabaseReference cdr = dbr.child("condition");
+        
+    }
+}
