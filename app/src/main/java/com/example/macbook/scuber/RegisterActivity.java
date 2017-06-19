f4b2688f299c56ae877f1c5c50c384e27d6f48b5
Lincoln White <lincoln.white@hva.nl>
2017-06-19 16:20:54 +0200

    register fragments made

--- app/src/main/java/com/example/macbook/scuber/RegisterActivity.java
+++ app/src/main/java/com/example/macbook/scuber/RegisterActivity.java
@@ -1,10 +1,19 @@
 package com.example.macbook.scuber;
 
+import android.app.FragmentManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v4.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -12,18 +21,61 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import layout.register1;
+import layout.register2;
+import layout.register3;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    ViewPager mViewPager;
+
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+/** set the adapter for ViewPager */
+        mViewPager.setAdapter(new SamplePagerAdapter(getSupportFragmentManager()));
+
         String email;
         String password;
         String passwordCheck;
     }
 
-    public void RegisterUser(String email, String password){
+
+    /**
+     * Defining a FragmentPagerAdapter class for controlling the fragments to be shown when user swipes on the screen.
+     */
+    public class SamplePagerAdapter extends FragmentPagerAdapter {
+
+        public SamplePagerAdapter(android.support.v4.app.FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            /** Show a Fragment based on the position of the current screen */
+            if (position == 0) {
+                return new register1();
+            } else if (position == 1) {
+                return new register2();
+            } else
+                return new register3();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 3;
+        }
+    }
+
+
+    public void RegisterUser(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
---
+++ app/src/main/java/layout/register2.java
@@ -0,0 +1,22 @@
+package layout;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.macbook.scuber.R;
+
+
+public class register2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_register2, container,
+                false);
+        return rootView;
+    }
+}
