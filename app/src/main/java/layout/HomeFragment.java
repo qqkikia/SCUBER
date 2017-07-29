ad99d3b34261c3e79d2929dcd0381c7c569af240
Lincoln White <lincoln.white@hva.nl>
2017-07-29 23:28:47 +0200

    mapsactivity pagersliding tabstrip made

--- app/src/main/java/layout/HomeFragment.java
+++ app/src/main/java/layout/HomeFragment.java
@@ -2,16 +2,10 @@ package layout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.astuetz.PagerSlidingTabStrip;
-import com.example.macbook.scuber.CardFragment;
 import com.example.macbook.scuber.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,6 +19,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     private GoogleMap mMap;
 
+    Boolean pickupNowBoolean;
+    Button pickupNow, pickupLater, flip;
+    EditText fromEditText, toEditText;
+    String fromString, toString;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +33,47 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        pickupNow = (Button) getView().findViewById(R.id.pickupNow);
+        pickupLater = (Button) getView().findViewById(R.id.pickupLater);
+        flip = (Button) getView().findViewById(R.id.flip);
+
+        fromEditText = (EditText) getView().findViewById(R.id.fromEditText);
+        toEditText = (EditText) getView().findViewById(R.id.toEditText);
+
+        pickupNowBoolean = true;
+
+        pickupNow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(pickupNowBoolean = false){
+                    pickupNow.setTextColor(getResources().getColor(R.color.darkBlue));
+                    pickupLater.setTextColor(getResources().getColor(R.color.grey));
+                    pickupNowBoolean = true;
+                }
+            }
+        });
+
+        pickupLater.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(pickupNowBoolean = true){
+                    pickupNow.setTextColor(getResources().getColor(R.color.grey));
+                    pickupLater.setTextColor(getResources().getColor(R.color.darkBlue));
+                    pickupNowBoolean = false;
+                }
+            }
+        });
+
+        flip.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                fromString = fromEditText.getText().toString();
+                toString = toEditText.getText().toString();
+
+                fromEditText.setText(toString);
+                toEditText.setText(fromString);
+            }
+        });
+
+
     }
 
 
